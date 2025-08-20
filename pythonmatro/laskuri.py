9d57c085ef49c5ac8327b3a2cb8cae088d9a69dc
NipaDelara <nipadelara@hotmail.com>
2025-08-20 14:32:30 +0300

    päivita

---
+++ pythonmatro/laskuri.py
@@ -0,0 +1,5 @@
+päivät = int(input("Kuinka monta päivä:  "))
+sekunnit = päivät * 24 * 60 * 60
+##print(" märää  sekunnti: ",sekunnit )
+##print(" märää  sekunnti: " +str(sekunnit ))
+print(f" märää  sekunnti: {sekunnit} ")
