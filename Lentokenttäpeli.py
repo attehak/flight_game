aaf269ee463258da008c3c06c9826e533f0b13a7
IR-Blaster <77672019+IR-Blaster@users.noreply.github.com>
2025-09-24 15:45:39 +0300

    lentokenttäpeli upd

--- Lentokenttäpeli.py
+++ Lentokenttäpeli.py
@@ -12,18 +12,26 @@ yhteys = mysql.connector.connect(
 )
 
 
+pelaaja_nimi = input("Hei, mikä on nimesi matkustaja?: ")
+time.sleep(1)
+print(f"Terve {pelaaja_nimi} ja tervetuloa seikkailemaan maailman ympäri")
+time.sleep(1)
+print(f"Ensimmäisenä valitse viidestä maasta (US, DE, BR, CH, AU) mieluisa, jonne haluat mennä")
+time.sleep(0.5)
+
+
 def hae_satunnainen_iso_lentokentta(maa):
-    sql = f"SELECT name, type, iso_country FROM airport WHERE iso_country = '{maa}' AND type = 'large_airport'"
+    sql = f"SELECT name, iso_country FROM airport WHERE iso_country = '{maa}' AND type = 'large_airport'"
     cursor = connection.cursor()
     cursor.execute(sql)
     lentokentat = cursor.fetchall()
 
     if lentokentat:
         satunnainen = random.choice(lentokentat)
-        print(f"Satunnainen iso lentokenttä maasta {maa}: {satunnainen[0]} ({satunnainen[1]})")
+        print(f"Lentokenttäsi valitusta maasta '{maa}' on tänään: {satunnainen[0]} ({satunnainen[1]})")
     else:
-        print(f"Ei löytynyt isoja lentokenttiä maasta {maa}.")
+        print(f"Kokeile kirjoittaa maa uudestaa, sillä maasta {maa} ei löytynyt isoa lentokenttää.")
 
-hae_satunnainen_iso_lentokentta(str(input("Anna maan koodi (esim. US): ")))
+hae_satunnainen_iso_lentokentta(str(input("Anna maa, kiitos (esim. US): ")))
 
 # hakee maa perusteella esim US iso_country taulua käyttämällä random lentokentän, jonka type on large airport.
