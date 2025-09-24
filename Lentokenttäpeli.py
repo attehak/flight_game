fd2f591349942598c394b0642a059fac655cfdae
IR-Blaster <77672019+IR-Blaster@users.noreply.github.com>
2025-09-24 15:12:11 +0300

    maan haku funktio

--- Lentokenttäpeli.py
+++ Lentokenttäpeli.py
@@ -11,3 +11,19 @@ yhteys = mysql.connector.connect(
     autocommit=True
 )
 
+
+def hae_satunnainen_iso_lentokentta(maa):
+    sql = f"SELECT name, type, iso_country FROM airport WHERE iso_country = '{maa}' AND type = 'large_airport'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    lentokentat = cursor.fetchall()
+
+    if lentokentat:
+        satunnainen = random.choice(lentokentat)
+        print(f"Satunnainen iso lentokenttä maasta {maa}: {satunnainen[0]} ({satunnainen[1]})")
+    else:
+        print(f"Ei löytynyt isoja lentokenttiä maasta {maa}.")
+
+hae_satunnainen_iso_lentokentta(str(input("Anna maan koodi (esim. US): ")))
+
+# hakee maa perusteella esim US iso_country taulua käyttämällä random lentokentän, jonka type on large airport.
