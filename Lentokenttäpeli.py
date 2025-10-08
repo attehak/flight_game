68314fcc428a6039f124b30541dac5a722475bcf
IR-Blaster <77672019+IR-Blaster@users.noreply.github.com>
2025-10-08 14:56:11 +0300

    kysely ruotsi

--- Lentokenttäpeli.py
+++ Lentokenttäpeli.py
@@ -38,6 +38,8 @@ world = """⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⣠⣀⡀⣀⣠⣤
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"""
 print(world)
 
+pelaaja_elämät = [1,2,3,4,5]
+
 print(f"Valitse maa mihin haluaisit mennä (US, GB, DE, CH, AU):")
 time.sleep(0.5)
 
@@ -73,6 +75,103 @@ def lomailu():
         time.sleep(1)
 
     print("Lomailu loppui!")
+
+
+def Ruotsi_kysely ():
+    print("Hejsan hur mår du?")
+    elämät = len(pelaaja_elämät)
+
+    while True:
+        print("A: Hä?")
+        print("B: Jag är bra, tack.")
+        print("C: Bastu bastu bastu!!!")
+        kysymys1 = input("Vastaa, joko A, B tai C: ")
+        time.sleep(2)
+        if kysymys1 == "A":
+            print("Sorry, can I have some kinda offical finnish identification?: ")
+            break
+        elif kysymys1 == "B":
+            print("Bra bra, har du en id?")
+            print("Ööööö yeees here i has!")
+            break
+        elif kysymys1 == "C":
+            elämät = elämät - 1
+            print("Very funny, can I have your ID")
+            time.sleep(1)
+            print(f"(kiusallista! Menetit yhden elämän, tämän seurauksena sinulla on {elämät} elämää.)")
+
+            break
+        else:
+            print("Vastaathan A, B tai C.")
+            time.sleep(2)
+            print("\n")
+            continue
+    time.sleep(2)
+    print("\nLooks good, a quick test to see if u pass our req")
+    print("So who is better at ice hockey Finland or Sweden?\n")
+
+    while True:
+        print("A: Finland ofc?")
+        print("B: You're dumb to even make a question than obvious.")
+        print("C: Jag är team svenska! ")
+        kysymys1 = input("Vastaa, joko A, B tai C: ")
+        time.sleep(2)
+        if kysymys1 == "A":
+            print("Fair I dont watch ice hockey anyways so you're good.")
+            break
+        elif kysymys1 == "B":
+            print("Language young one, you failed this test by losing your cool!")
+            elämät = elämät - 1
+            print(f"(Ei voi olla! Menetit yhden elämän, tämän seurauksena sinulla on {elämät} elämää.)")
+            break
+        elif kysymys1 == "C":
+            elämät = elämät + 1
+            print("Bra, you passed this test with flying colors or smth like that")
+            time.sleep(1)
+            print(f"Joskus kannattaa olla humble tai Ruotsi fani, tämän seurauksena sinulla on {elämät} elämää.")
+
+            break
+        else:
+            print("Vastaathan A, B tai C.")
+            time.sleep(2)
+            print("\n")
+            continue
+
+    time.sleep(2)
+    print("\n TEEVETUULUA to sweden, but frfr final question")
+    print("Are you enjoying this game thus far!?\n")
+
+
+    while True:
+        print("A: Its aight, it works for now and I feel like im one with the game")
+        print("B: Yes I would pay money for this artpiece!")
+        print("C: Nah fam, this piece of game is horrible and devs shouldn't continue this prototype.")
+        kysymys1 = input("Vastaa, joko A, B tai C: ")
+        time.sleep(2)
+        if kysymys1 == "A":
+            print("Thanks g, welcome to Sweden!")
+            break
+        elif kysymys1 == "B":
+            print("Bless, heres a life for W glaze!")
+            elämät = elämät + 1
+            print(f"(+glaze, tämän seurauksena sinulla on {elämät} elämää.)")
+            break
+        elif kysymys1 == "C":
+            elämät = elämät - 1
+            print("Yo, these devs work hard for your enjoyment chill bro")
+            time.sleep(1)
+            print(f"- aura, jonka seurauksena elämäsi on {elämät}.")
+
+            break
+        else:
+            print("Vastaathan A, B tai C.")
+            time.sleep(2)
+            print("\n")
+            continue
+
+            
+
+
 
 hae_satunnainen_iso_lentokentta(str(input("Anna maa, kiitos (esim. US): ")))
 # hakee maa perusteella esim US iso_country taulua käyttämällä random lentokentän, jonka type on large airport.
