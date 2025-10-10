2c17943f299bcb8cda9c080aea458750e4f6f2e5
NipaDelara <nipadelara@hotmail.com>
2025-10-10 13:35:07 +0300

    update_sql_quries

---
+++ FlightGame/Metropoli-ohjelmointi1-python-project-/src/db/flight_game.py
@@ -0,0 +1,12 @@
+﻿import mariadb
+
+def get_connection():
+    return mariadb.connect(
+        host="127.0.0.1",
+        user="root",          
+        password="12345",     
+        database="flight_game",
+        port=3307
+    )
+
+
