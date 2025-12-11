8e175c6521db98809f14ab47b9804388dc03418b
Atte <atte.hakkila@gmail.com>
2025-12-11 17:55:48 +0200

    Add files via upload

---
+++ Projekti 2/koodit/game/airport.py
@@ -0,0 +1,17 @@
+import random
+from app import db
+
+def get_random_large_airport(country_code):
+    sql = (
+        "SELECT name, iso_country "
+        "FROM airport "
+        "WHERE iso_country = %s AND type = 'large_airport'"
+    )
+    cursor = db.cursor()
+    cursor.execute(sql, (country_code,))
+    results = cursor.fetchall()
+
+    if not results:
+        return None
+
+    return random.choice(results)
